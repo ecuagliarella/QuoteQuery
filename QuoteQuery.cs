6e444df7be838065fcdb0d409c22d4e064e4c9ff
unknown <ecuagliarella@gmail.com>
2019-06-16 16:47:18 -0300

    Initial commit

---
+++ QuoteQuery.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace QuoteQuery {
+
+	public class Quote {
+
+		public string text {get; set;}
+		public string author {get; set;}
+
+	}
+
+	public static class QuoteQuery {
+
+		static QuoteQuery() {
+
+			// Parsing quotes from json.
+			using (StreamReader streamReader = new StreamReader("quotes.json")) {
+
+				quotes = JsonConvert.DeserializeObject<Quote[]>(streamReader.ReadToEnd());
+
+			}
+
+			random = new Random();
+
+		}
+
+		[FunctionName("QuoteQuery")]
+		public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log) {
+
+			log.LogInformation("[START] Processing a request.");
+
+			string response = "Welcome! ";
+
+			int quoteNumber;
+
+			// Obtaining parameter.
+			string quoteQuery = req.Query["quote"];
+
+			log.LogInformation("[INPUT] \"" + quoteQuery + "\"");
+
+			// Checking parameter.
+			if (string.IsNullOrEmpty(quoteQuery)) {
+
+				quoteNumber = random.Next(quotes.Length) + 1;
+				response += "Here's a random quote for you:";
+
+			} else if (Int32.TryParse(quoteQuery, out quoteNumber) && quoteNumber > 0 && quoteNumber <= quotes.Length) {
+
+				response += "Here's your requested quote:";
+
+			} else {
+
+				log.LogError("[ERROR] Invalid input.");
+
+				return new BadRequestObjectResult(response + "Couldn't find specified quote, please enter a number between 1 and " + quotes.Length + ".");
+
+			}
+
+			// Finishing response.
+			Quote selectedQuote = quotes[quoteNumber - 1];
+			response += "\n\n\t[QUOTE " + quoteNumber + "] \"" + selectedQuote.text + "\" -" + selectedQuote.author;
+
+			log.LogInformation("[OUTPUT] \"" + response + "\"");
+
+			// Returning response.
+			return new OkObjectResult(response);
+
+		}
+
+		private static Quote[] quotes;
+		private static Random random;
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# QuoteQuery - Azure Functions Demo Project
+Demo project for the "Getting Started with Azure Functions on Docker!" tutorial.
+
+Displays an inspiring famous quote.
+
+If no [quote] parameter specified, returns a random quote.
+
+Find the blog post here: https://medium.com/@ecuagliarella/getting-started-with-azure-functions-on-docker-e22da5be2a49
